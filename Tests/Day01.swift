e64897b3740ce17141008b9ffb318b7f5730532f
Emre <degirmenci.a.emre@gmail.com>
2024-12-01 18:42:47 +0300

    Update Day 01 Part 2 Tests

--- Tests/Day01.swift
+++ Tests/Day01.swift
@@ -4,33 +4,22 @@ import Testing
 
 // Make a copy of this file for every day to ensure the provided smoke tests
 // pass.
-struct Day00Tests {
+struct Day01Tests {
   // Smoke test data provided in the challenge question
-  let testData = """
-    1000
-    2000
-    3000
 
-    4000
-
-    5000
-    6000
-
-    7000
-    8000
-    9000
-
-    10000
-
-    """
+  let testData =  """
+        1 2
+        3 4
+        5 6
+        """
 
   @Test func testPart1() async throws {
-    let challenge = Day00(data: testData)
-    #expect(String(describing: challenge.part1()) == "6000")
+    let challenge = Day01(data: testData)
+    #expect(String(describing: challenge.part1()) == "3")
   }
 
   @Test func testPart2() async throws {
-    let challenge = Day00(data: testData)
-    #expect(String(describing: challenge.part2()) == "32000")
+    let challenge = Day01(data: testData)
+    #expect(String(describing: challenge.part2()) == "0")
   }
 }
