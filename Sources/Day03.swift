60901f083fcf9896d829e27087eb094f6dec82e5
Emre <degirmenci.a.emre@gmail.com>
2024-12-03 20:21:20 +0300

    Day 03 Part 1 and 2 Solved

--- Sources/Day03.swift
+++ Sources/Day03.swift
@@ -1,27 +1,92 @@
 import Algorithms
-import Parsing
+import Foundation
+import RegexBuilder
 
-// MARK: - Solution
 struct Day03: AdventDay {
-    var data: String
+  var data: String
+  
+  struct Mul: Equatable {
+    let x: Int
+    let y: Int
+    var result: Int { x * y }
+  }
+  
+  enum Instruction {
+    case multiply(Mul)
+    case enable    // do()
+    case disable   // don't()
+  }
+  
+  func parseInstructions() -> [Instruction] {
+    var instructions: [Instruction] = []
+    var position = data.startIndex
     
-    func part1() -> Any {
-        do {
-            let reports = try ReportsParser().parse(data[...])
-            return reports.filter { $0.isSafe() }.count
-        } catch {
-            print("Error parsing input: \(error)")
-            return 0
+    while position < data.endIndex {
+      let remaining = String(data[position...])
+      
+      if let range = remaining.range(of: #"do\(\)"#, options: .regularExpression) {
+        instructions.append(.enable)
+        position = data.index(position, offsetBy: range.upperBound.utf16Offset(in: remaining))
+        continue
+      }
+      
+      if let range = remaining.range(of: #"don't\(\)"#, options: .regularExpression) {
+        instructions.append(.disable)
+        position = data.index(position, offsetBy: range.upperBound.utf16Offset(in: remaining))
+        continue
+      }
+      
+      if let range = remaining.range(of: #"mul\((\d{1,3}),(\d{1,3})\)"#, options: .regularExpression),
+         let match = try? NSRegularExpression(pattern: #"mul\((\d{1,3}),(\d{1,3})\)"#)
+        .firstMatch(in: remaining, range: NSRange(location: 0, length: remaining.count)) {
+        
+        if let xRange = Range(match.range(at: 1), in: remaining),
+           let yRange = Range(match.range(at: 2), in: remaining),
+           let x = Int(remaining[xRange]),
+           let y = Int(remaining[yRange]),
+           (1...999).contains(x),
+           (1...999).contains(y) {
+          instructions.append(.multiply(Mul(x: x, y: y)))
         }
+        position = data.index(position, offsetBy: range.upperBound.utf16Offset(in: remaining))
+        continue
+      }
+      
+      position = data.index(after: position)
     }
     
-    func part2() -> Any {
-        do {
-            let reports = try ReportsParser().parse(data[...])
-            return reports.filter { $0.isSafe(withDampener: true) }.count
-        } catch {
-            print("Error parsing input: \(error)")
-            return 0
+    return instructions
+  }
+  
+  func part1() -> Int {
+    return parseInstructions()
+      .compactMap { instruction -> Int? in
+        if case .multiply(let mul) = instruction {
+          return mul.result
         }
+        return nil
+      }
+      .reduce(0, +)
+  }
+  
+  func part2() -> Int {
+    let instructions = parseInstructions()
+    var isEnabled = true
+    var sum = 0
+    
+    for instruction in instructions {
+      switch instruction {
+      case .multiply(let mul):
+        if isEnabled {
+          sum += mul.result
+        }
+      case .enable:
+        isEnabled = true
+      case .disable:
+        isEnabled = false
+      }
     }
+    
+    return sum
+  }
 }
--- Tests/Day03.swift
+++ Tests/Day03.swift
@@ -1,5 +1,6 @@
 import Testing
 import Foundation
+import Parsing
 @testable import AdventOfCode
 
 // Make a copy of this file for every day to ensure the provided smoke tests
@@ -7,30 +8,18 @@ import Foundation
 struct Day03Tests {
   // Smoke test data provided in the challenge question
   // The following test data also included in Day02.txt file and I have an helper to read from Bundle and test it
-  func loadTestData() throws -> String {
-    // For testing with embedded data
-    let embeddedData = """
-          7 6 4 2 1
-          1 2 7 8 9
-          9 7 6 2 1
-          1 3 2 4 5
-          8 6 4 4 1
-          1 3 6 7 9
-          """
-    return embeddedData
+  
+  @Test func testPart1() async throws {
+    let challenge = Day03(data: "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")
+    #expect(String(describing: challenge.part1()) == "161")
   }
   
-  @Test
-  func testPart1() async throws {
-    let testData = try loadTestData()
-    let challenge = Day03(data: testData)
-    #expect(String(describing: challenge.part1()) == "2")
-  }
-  
-  @Test
-  func testPart2() async throws {
-    let testData = try loadTestData()
-    let challenge = Day02(data: testData)
-    #expect(String(describing: challenge.part2()) == "4")
+  @Test func testPart2() async throws {
+    let input =
+    """
+xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+"""
+    let challenge = Day03(data: input)
+    #expect(String(describing: challenge.part2()) == "40")
   }
 }
